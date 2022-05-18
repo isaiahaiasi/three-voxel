db865ea7e9d05096bd0bc12929681c057c55bbe6
Isaiah A <isaiah.althouse@gmail.com>
2022-05-18 09:29:13 -0700

    Fix default camera positioning

--- src/VoxelCanvas.tsx
+++ src/VoxelCanvas.tsx
@@ -6,6 +6,20 @@ import VoxelWorldRenderer from "./VoxelWorldRenderer";
 import { useContext } from "react";
 import ActiveVoxelContext from "./contexts/ActiveVoxelContext";
 import { Stats } from "@react-three/drei";
+import { Vector3 } from "three";
+
+const CHUNK_SIZE = 32;
+
+const cameraDefaults = {
+  fov: 75,
+  near: 0.1,
+  far: 1000,
+  position: new Vector3(
+    CHUNK_SIZE,
+    CHUNK_SIZE,
+    CHUNK_SIZE,
+  ),
+};
 
 export default function VoxelCanvas() {
   const selectedVoxelRef = useContext(ActiveVoxelContext);
@@ -15,16 +29,19 @@ export default function VoxelCanvas() {
     return null;
   }
 
-  return <Canvas id="c">
+  return <Canvas id="c" camera={cameraDefaults}>
     <color attach="background" args={["black"]}/>
     <fog attach="fog" color={0x101040} near={1} far={125}/>
 
-    <Controls/>
+    <Controls chunkSize={CHUNK_SIZE}/>
     <Lighting/>
 
     <primitive object={new THREE.AxesHelper(10)}/>
 
-    <VoxelWorldRenderer selectedVoxelRef={selectedVoxelRef}/>
+    <VoxelWorldRenderer
+      selectedVoxelRef={selectedVoxelRef}
+      chunkSize={CHUNK_SIZE}
+    />
 
     <Stats/>
   </Canvas>
--- src/three-helper-components/Controls.tsx
+++ src/three-helper-components/Controls.tsx
@@ -3,9 +3,17 @@ import { OrbitControls } from '@react-three/drei';
 const DEFAULT_MIN_DISTANCE = 1;
 const DEFAULT_MAX_DISTANCE = 2000;
 
-export default function Controls() {
+interface ControlsProps {
+  chunkSize: number;
+}
+
+export default function Controls({chunkSize}:ControlsProps) {
   return <OrbitControls
-    target={[0, 0, 0]}
+    target={[
+      chunkSize / 2,
+      chunkSize / 3,
+      chunkSize / 2,
+    ]}
     minDistance={DEFAULT_MIN_DISTANCE}
     maxDistance={DEFAULT_MAX_DISTANCE}
     enableDamping
